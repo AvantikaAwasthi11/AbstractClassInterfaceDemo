226e67eeb086c8950cf77fef301627db2c0886af
u31d74 <u31d74@INGURWN110444>
2023-11-02 08:17:55 +0530

    Add project files.

---
+++ AbstractClassInterfaceDemo/NewClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassInterfaceDemo
+{
+    internal class NewClass
+    {
+        Shape shape=new Shape();
+
+
+
+
+
+
+        class CheckShape
+        {
+            public void checkShape(Shape shape)
+            {
+                if(shape is Square)
+                {
+                    shape=(Square)shape;
+                }
+                else if(shape is Rectangle)
+                {
+                    shape = (Rectangle)shape;
+                }
+                shape.GetDimensions();
+
+            }
+        }
+    }
+}
